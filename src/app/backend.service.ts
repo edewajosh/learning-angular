151161dbaf5491078e0750009e56c4ca090c6c32
edewa <joshedewa@gmail.com>
2020-06-25 22:11:56 +0300

    - Added CRUD product services and displayed them

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,18 +1,70 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from './../environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 import { Product } from '../app/components/product'
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
-  apiUrl : environment.apiUrl;
+  apiUrl = environment.apiUrl;
+ httpOptions = {
+   headers: new HttpHeaders({
+     'Content-Type': 'application/json'
+   })
+ }
   constructor( private httpClient: HttpClient) { }
 
+  // GET: returns all the products
   getProducts(): Observable<Product[]>{
-    return this.httpClient.get<Product[]>(this.apiUrl);
+    return this.httpClient.get<Product[]>(`${this.apiUrl}/products`)
+    .pipe(
+      catchError(this.errorHandler)
+    );
   }
 
+  // GET: returns a single product that matches the id provided
+  getProductById(id): Observable<Product>{
+    return this.httpClient.get<Product>(`${this.apiUrl}/products/${id}`)
+  }
+
+  // POST: request to create a product
+  createproduct(product): Observable<Product> {
+    return this.httpClient.post<Product>(`${this.apiUrl}/products`, JSON.stringify(product), this.httpOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  // PUT: function to an exisiting product
+  updateProduct(id, product): Observable<Product> {
+    return this.httpClient.post<Product>(`${this.apiUrl}/products/${id}`, JSON.stringify(product), this.httpOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  // DELETE: function to delete a product with that particular ID
+  deleteProduct(id): Observable<Product> {
+    return this.httpClient.delete<Product>(`${this.apiUrl}/products/${id}`)
+    .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  errorHandler(error) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent){
+      // Get client-side error
+      errorMessage = error.error.message;
+    }else {
+      // Get server side error
+      errorMessage = `Error code: ${error.status}\nMessage : ${error.error}`
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { BackendService } from 'src/app/backend.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = []
+
+  constructor(
+    private productService: BackendService
+  ) { }
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe((data: Product[]) =>{
+      console.log(data);
+      this.products = data;
+    })
   }
 
 }
